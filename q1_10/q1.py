c37a69f1380677215e835055ccc3d880c2e5669a
xichen1 <xichen.pan@gmail.com>
2020-04-04 03:36:23 -0600

    initial commit

---
+++ q1_10/q1.py
@@ -0,0 +1,17 @@
+import cv2
+def rgb2bgr(img):
+
+	red = img[:,:,2].copy()
+	green = img[:,:,1].copy()
+	blue = img[:,:,0].copy()
+
+	img[:,:,0] = red
+	img[:,:,1] = green
+	img[:,:,2] = blue
+	return img
+img = cv2.imread("imori.jpg")
+res = rgb2bgr(img)
+cv2.imwrite("result1.jpg",res)
+cv2.imshow("result", res)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ q1_10/q2.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+def grayscale(img):
+    b = img[:,:,0]
+    g = img[:,:,1]
+    r = img[:,:,2]
+    result = 0.2126*r + 0.7152*g + 0.0722*b
+    print(result[0])
+    result = result.astype(np.uint8) # convert the float to uint8 because cv2 cannot imwrite float img
+    print(result[0])
+    return result
+
+def main():
+	img = cv2.imread("imori.jpg")
+	out = grayscale(img)
+	cv2.imwrite("out.jpg", out)
+	cv2.imshow("result", out)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+main()
